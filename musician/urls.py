8f480fe2b264a18051c16a5b254e64f11460e299
nmkhans <nurmoin05@gmail.com>
2024-06-18 20:38:33 +0600

    add musician page created

--- musician/urls.py
+++ musician/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-  
+  path('add-musician/', views.add_musician, name = 'add-musician')
 ]
--- musician/views.py
+++ musician/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def add_musician(req):
+  return render(req, 'musician/add_musician.html')
