55af75c80bff266fe3951d9a41dc7742f88e7dc6
nmkhans <nurmoin05@gmail.com>
2024-06-18 21:21:13 +0600

    add musician and edit musician feature added

--- musician/urls.py
+++ musician/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('add-musician/', views.add_musician, name = 'add-musician')
+  path('add-musician/', views.add_musician, name = 'add-musician'),
+  path('edit-musician/<int:id>', views.edit_musician, name = 'edit-musician')
 ]
--- musician/views.py
+++ musician/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import MusicianForm
+from .models import Musician
 
 # Create your views here.
 def add_musician(req):
-  return render(req, 'musician/add_musician.html')
+  if req.method == 'POST':
+    form = MusicianForm(req.POST)
+    
+    if form.is_valid():
+      form.save()
+      return redirect('home')
+  else:
+    form = MusicianForm()
+
+  return render(req, 'musician/add_musician.html', {'form': form})
+
+def edit_musician(req, id):
+  musician = Musician.objects.get(pk = id)
+
+  if req.method == 'POST':
+    form = MusicianForm(req.POST, instance = musician)
+
+    if form.is_valid():
+      form.save()
+      return redirect('home')
+  else:
+    form = MusicianForm(instance = musician)
+
+  return render(req, 'musician/edit_musician.html', {'form': form})
