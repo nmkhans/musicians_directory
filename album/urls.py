9459497f3fe28579d4b375c7b235931f2661c33c
nmkhans <nurmoin05@gmail.com>
2024-06-18 23:53:04 +0600

    edit album added

--- album/urls.py
+++ album/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('add-album/', views.add_album, name = 'add-album')
+  path('add-album/', views.add_album, name = 'add-album'),
+  path('edit-album/<int:id>', views.edit_album, name = 'edit-album'),
 ]
--- album/views.py
+++ album/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import AlbumForm
+from .models import Album
 
 # Create your views here.
 def add_album(req):
@@ -13,3 +14,16 @@ def add_album(req):
     form = AlbumForm()
 
   return render(req, 'album/add_album.html', {'form': form})
+
+def edit_album(req, id):
+  album = Album.objects.get(pk = id)
+  if req.method == 'POST':
+    form = AlbumForm(req.POST, instance = album)
+
+    if form.is_valid():
+      form.save()
+      return redirect('home')
+  else:
+    form = AlbumForm(instance = album)
+
+  return render(req, 'album/edit_album.html', {'form': form})
